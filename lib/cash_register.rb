0f25c3cf50d2c5d7422eeb8e260834bf6db9a892
Greg Morgan <gregm9999@gmail.com>
2018-11-10 03:34:35 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -11,14 +11,9 @@ class CashRegister
   end
 
   def add_item(title, price, quantity=1)
-    if quantity>1
-      i=0
-      while i<quantity
-        @items<<title
-        i+=1
-      end
-    else
-      @items<<title
+    quantity.times do
+      items << title
+    end
     end
     @total+=price*quantity
     @last_transaction_amount=@total
